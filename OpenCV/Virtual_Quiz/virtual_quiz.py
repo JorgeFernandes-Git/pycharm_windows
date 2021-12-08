d77c15f89094833cae20e9e26ef74d3369ee7f76
Jorge_Fernandes <jorge.andre.silva.fernandes@gmail.com>
2021-12-08 15:27:37 +0000

    cursor on hand

--- OpenCV/Virtual_Quiz/virtual_quiz.py
+++ OpenCV/Virtual_Quiz/virtual_quiz.py
@@ -2,6 +2,7 @@ import csv
 import cv2
 import cvzone
 from cvzone.HandTrackingModule import HandDetector
+import time
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
@@ -19,6 +20,13 @@ class QuestionClass:
         self.answer = int(data[5])  # to verify if it's correct answer
 
         self.user_answer = None  # verify if the user choose a answer
+
+    def update(self, cursor, bboxs):
+        for x, bbox in enumerate(bboxs):
+            x1, y1, x2, y2 = bbox
+            if x1 < cursor[0] < x2 and y1 < cursor[1] < y2:
+                self.user_answer = x + 1  # x + 1 because of the csv format
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), cv2.FILLED)
 
 
 # import csv file data
@@ -40,9 +48,32 @@ while True:
     img = cv2.flip(img, 1)
     hands, img = detector.findHands(img, flipType=False)
 
-    question_simple = questions_list[0]
-    img, bbox = cvzone.putTextRect(img, question_simple.question, [100, 100], 2, 2, offset=50, border=5,
-                                   colorB=(255, 255, 255), colorR=(0, 0, 0))
+    # iterate through all questions
+    if q_num < q_total:
+        question_simple = questions_list[q_num]
+        img, bbox = cvzone.putTextRect(img, question_simple.question, [100, 100], 2, 2, offset=50, border=5,
+                                       colorB=(255, 255, 255), colorR=(0, 0, 0), colorT=(255, 255, 255))
+        img, bbox1 = cvzone.putTextRect(img, question_simple.choice1, [100, 250], 2, 2, offset=50, border=5,
+                                        colorB=(255, 255, 255), colorR=(0, 0, 0), colorT=(255, 255, 255))
+        img, bbox2 = cvzone.putTextRect(img, question_simple.choice2, [400, 250], 2, 2, offset=50, border=5,
+                                        colorB=(255, 255, 255), colorR=(0, 0, 0), colorT=(255, 255, 255))
+        img, bbox3 = cvzone.putTextRect(img, question_simple.choice3, [100, 400], 2, 2, offset=50, border=5,
+                                        colorB=(255, 255, 255), colorR=(0, 0, 0), colorT=(255, 255, 255))
+        img, bbox4 = cvzone.putTextRect(img, question_simple.choice4, [400, 400], 2, 2, offset=50, border=5,
+                                        colorB=(255, 255, 255), colorR=(0, 0, 0), colorT=(255, 255, 255))
+
+        if hands:  # hands are from the method findHands
+            lm_list = hands[0]["lmList"]
+            cursor = lm_list[8]  # tip of the index by mediapipe
+            length, info = detector.findDistance(lm_list[8], lm_list[12])
+
+            # click mode
+            if length < 40:
+                question_simple.update(cursor, [bbox1, bbox2, bbox3, bbox4])
+                print(question_simple.user_answer)
+                if question_simple.user_answer is not None:
+                    time.sleep(0.3)
+                    q_num += 1
 
     cv2.imshow("Image", img)
 

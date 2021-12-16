97d2770a7bd07092b4f3a2a7b8656dbf3ec699de
Jorge_Fernandes <jorge.andre.silva.fernandes@gmail.com>
2021-12-16 01:12:23 +0000

    add transparency and display text

--- OpenCV/AI_Keyboard/AI_Keyboard.py
+++ OpenCV/AI_Keyboard/AI_Keyboard.py
@@ -1,6 +1,7 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 
+
 # mediapipe 0.8.7
 # cvzone 1.4.1
 
@@ -12,11 +13,38 @@ class Button:
         self.text = text
 
     def draw(self, img):
-        cv2.rectangle(img, self.pos, (self.pos[0] + self.size[0], self.pos[1] + self.size[1]), (255, 0, 255),
+        overlay = img.copy()
+        cv2.rectangle(overlay, self.pos, (self.pos[0] + self.size[0], self.pos[1] + self.size[1]), (255, 0, 255),
                       cv2.FILLED)
+        alpha = 0.4
+        img = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)  # add transparency
+        cv2.rectangle(img, self.pos, (self.pos[0] + self.size[0], self.pos[1] + self.size[1]), (0, 0, 0),
+                      2)
         cv2.putText(img, self.text, (self.pos[0] + 20, self.pos[1] + 65),
                     cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
         return img
+
+    def click_check(self, x, y):
+        if self.pos[0] < x < self.pos[0] + self.size[0] and self.pos[1] < y < self.pos[1] + self.size[1]:
+            cv2.rectangle(img, self.pos, (self.pos[0] + self.size[0], self.pos[1] + self.size[1]), (0, 255, 0),
+                          cv2.FILLED)
+            cv2.rectangle(img, self.pos, (self.pos[0] + self.size[0], self.pos[1] + self.size[1]), (0, 0, 0),
+                          2)
+            cv2.putText(img, self.text, (self.pos[0] + 20, self.pos[1] + 65),
+                        cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
+            return True
+        else:
+            return False
+
+    def over_key(self, x, y):
+        if self.pos[0] < x < self.pos[0] + self.size[0] and self.pos[1] < y < self.pos[1] + self.size[1]:
+            cv2.rectangle(img, (self.pos[0], self.pos[1]), (self.pos[0] + self.size[0], self.pos[1] + self.size[1]),
+                          (175, 0, 175),
+                          cv2.FILLED)
+            cv2.rectangle(img, self.pos, (self.pos[0] + self.size[0], self.pos[1] + self.size[1]), (0, 0, 0),
+                          2)
+            cv2.putText(img, btn.text, (self.pos[0] + 20, self.pos[1] + 65),
+                        cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
 
 
 cap = cv2.VideoCapture(0)
@@ -32,17 +60,70 @@ keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
 btn_list = []
 for x in range(0, 10):  # ten horizontal keys
     for y in range(0, 3):  # three rows
-        btn_list.append(Button([100 * x + 150, 100*y+100], keys[y][x]))
+        btn_list.append(Button([100 * x + 150, 100 * y + 100], keys[y][x]))
+
+# ------------------------------- variables
+delay_cnt = 0
+write_text = ""
 
 while True:
     success, img = cap.read()
+    img = cv2.flip(img, 1)  # 0 - x, 1 - y
 
-    img = detector.findHands(img)
-    lm_list, bbox_info = detector.findPosition(img)
+    # hand detection
+    hands, img = detector.findHands(img, flipType=False)
 
+    # ------------------------------- draw buttons
     for btn in btn_list:
         img = btn.draw(img)
 
+    # create write area
+    overlay = img.copy()
+    cv2.rectangle(overlay, (150, 600), (150 + 1000, 600 + 85), (255, 0, 255), cv2.FILLED)
+    alpha = 0.4
+    img = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)  # add transparency
+    cv2.rectangle(img, (150, 600), (150 + 1000, 600 + 85), (0, 0, 0), 2)
+
+    # ------------------------------- check for hand
+    if hands:
+        lm_list = hands[0]["lmList"]
+        length, _, img = detector.findDistance(lm_list[8], lm_list[12], img)  # dist between index and middle fingertips
+        fingers = detector.fingersUp(hands[0])
+        # print(*fingers)
+
+        x_tip, y_tip = lm_list[8]  # locations of the tip in x_tip and y
+        # print(length)
+
+        if length < 40:
+            for btn in btn_list:
+                if btn.click_check(x_tip, y_tip) and delay_cnt == 0:  # true from the method
+                    my_value = btn.text  # get the clicked key in the list
+                    # print(my_value)
+                    write_text += my_value
+                    delay_cnt = 1  # delay to avoid duplication
+        else:
+            for btn in btn_list:
+                btn.over_key(x_tip, y_tip)
+
+        # clear text -- close hand
+        if fingers == [0, 0, 0, 0, 0]:
+            write_text = ""
+
+        # add space -- lift index and pinky finger
+        if fingers == [0, 1, 0, 0, 1] and delay_cnt == 0:
+            write_text += " "
+            delay_cnt = 1
+
+            # ------------------------------- avoid duplication
+    if not delay_cnt == 0:
+        delay_cnt += 1
+        if delay_cnt > 10:
+            delay_cnt = 0
+
+    # ------------------------------- display text
+    cv2.putText(img, write_text, (160, 650), cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
+
+    # ------------------------------- display image
     cv2.imshow("image", img)
 
     # ESC to close

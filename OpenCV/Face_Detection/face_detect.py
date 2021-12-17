369c7ef3d405ffeaee364710343a44d33aec7271
Jorge_Fernandes <jorge.andre.silva.fernandes@gmail.com>
2021-12-17 04:04:35 +0000

    update

--- OpenCV/Face_Detection/face_detect.py
+++ OpenCV/Face_Detection/face_detect.py
@@ -10,8 +10,8 @@ haar_cascade = cv2.CascadeClassifier("haar_face.xml")
 
 faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
 
-for (x,y,w,h) in faces_rect:
-    cv2.rectangle(img,(x,y), (x+w,y+h),(0,255,0),thickness=2)
+for (x, y, w, h) in faces_rect:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
 
 cv2.imshow("Detected Faces", img)
 
--- OpenCV/Face_Train/faces_train.py
+++ OpenCV/Face_Train/faces_train.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 
 DIR = r'D:\Projetos\Pycharm\pycharm_windows\OpenCV\Face_Train\images'
+haar_cascade = cv2.CascadeClassifier("haar_face.xml")
 
 people = []
 for i in os.listdir(DIR):
@@ -14,7 +15,7 @@ labels = []
 
 def create_train():
     for person in people:
-        path = os.path.join(DIR,person)
+        path = os.path.join(DIR, person)
         label = people.index(person)
 
         for img in os.listdir(path):
@@ -23,3 +24,12 @@ def create_train():
             img_array = cv2.imread(img_path)
             gray = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
 
+            faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+
+            for (x, y, w, h) in faces_rect:
+                faces_roi = gray[y:y + h, x:x + w]  # region of interest ROI
+                features.append(faces_roi)
+                labels.append(label)
+
+
+create_train()

7dc7aaf7271d97ae04f61c224740bd4983dde98c
Jorge_Fernandes <jorge.andre.silva.fernandes@gmail.com>
2021-12-18 11:55:04 +0000

    update

--- OpenCV/Virtual_Drag/virtual_drag.py
+++ OpenCV/Virtual_Drag/virtual_drag.py
@@ -1,12 +1,87 @@
 import cv2
+import cvzone
+from cvzone.HandTrackingModule import HandDetector
+import os
+
+
+class DragImage:
+    def __init__(self, path, pos_origin, type):
+        self.path = path
+        self.pos_origin = pos_origin
+        self.type = type
+
+        if self.type == "png":
+            self.img = cv2.imread(self.path, cv2.IMREAD_UNCHANGED)
+        else:
+            self.img = cv2.imread(self.path)
+
+        self.img = cv2.resize(self.img, (273, 286))  # resize images
+        self.size = self.img.shape[:2]  # only the first 2 parameters
+
+    def update(self, cursor):
+        ox, oy = self.pos_origin
+        h, w = self.size
+
+        #         # check if cursor is in image region
+        if ox < cursor[0] < ox + w and oy < cursor[1] < oy + h:
+            self.pos_origin = cursor[0] - w // 2, cursor[1] - h // 2
+
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)  # width
 cap.set(4, 720)  # height
+detector = HandDetector(detectionCon=0.8)
+
+# img_move = cv2.imread("Images/linux.png", cv2.IMREAD_UNCHANGED)
+# img_move = cv2.resize(img_move, (273, 286))
+# ox, oy = 200, 200
+
+path = 'Images'
+my_list = os.listdir(path)
+# print(my_list)
+
+img_list = []
+for i, path_img in enumerate(my_list):
+    if "png" in path_img:
+        img_type = "png"
+    else:
+        img_type = "jpg"
+    img_list.append(DragImage(f'{path}/{path_img}', [i * 200 + 50, 50], img_type))
 
 while True:
     _, img = cap.read()
     img = cv2.flip(img, 1)
+    # h, w, _ = img_move.shape
+
+    # hand detection
+    hands, img = detector.findHands(img, flipType=False)
+
+    if hands:
+        lm_list = hands[0]["lmList"]
+
+        length, info, img = detector.findDistance(lm_list[8], lm_list[12], img)
+        # print(length)
+
+        if length < 50:  # check is click
+            cursor = lm_list[8]
+            for img_obj in img_list:
+                img_obj.update(cursor)
+
+    try:
+
+        for img_obj in img_list:
+            h, w = img_obj.size
+            ox, oy = img_obj.pos_origin
+
+            if img_obj.type == "png":
+                # PNG images
+                img = cvzone.overlayPNG(img, img_obj.img, [ox, oy])
+            else:
+                # JPG images
+                img[oy:oy + h, ox:ox + w] = img_obj.img
+
+    except ValueError:
+        pass
 
     cv2.imshow("image", img)
 
--- OpenCV/Virtual_Zoom/virtual_zoom.py
+++ OpenCV/Virtual_Zoom/virtual_zoom.py
@@ -1,4 +1,5 @@
 import cv2
+import cvzone
 from cvzone.HandTrackingModule import HandDetector
 
 # init
@@ -17,8 +18,8 @@ while True:
     _, img = cap.read()
     img = cv2.flip(img, 1)
     hands = detector.findHands(img, draw=False)
-    zoom_img = cv2.imread('open_cv.png')
-    zoom_img = cv2.resize(zoom_img, (250, 250))
+    zoom_img = cv2.imread('linux.png', cv2.IMREAD_UNCHANGED)
+    zoom_img = cv2.resize(zoom_img, (273, 286))
 
     if len(hands) == 2:
         if detector.fingersUp(hands[0]) == [1, 1, 0, 0, 0] and detector.fingersUp(hands[1]) == [1, 1, 0, 0, 0]:
@@ -40,11 +41,19 @@ while True:
         start_dist = None
 
     try:
+
         h1, w1, _ = zoom_img.shape
         new_h1, new_w1 = ((h1 + scale)//2)*2, ((w1 + scale)//2)*2  # this will prevent the lost of a pixel
-        zoom_img = cv2.resize(zoom_img, (new_w1, new_h1))
 
-        img[cy - new_h1 // 2:cy + new_h1 // 2, cx - new_w1 // 2:cx + new_w1 // 2] = zoom_img
+        # prevent image too small
+        if new_h1 > 0 and new_w1 > 0:
+            zoom_img = cv2.resize(zoom_img, (new_w1, new_h1))
+        else:
+            new_h1, new_w1 = 1, 1
+
+        img = cvzone.overlayPNG(img, zoom_img, [cx - new_w1 // 2, cy - new_h1 // 2])
+        # img[cy - new_h1 // 2:cy + new_h1 // 2, cx - new_w1 // 2:cx + new_w1 // 2] = zoom_img
+
     except ValueError:  # prevent image out of screen
         pass
 

66c4b27647a3dfff7eb77ebf5713b6a038c4a3df
Jorge_Fernandes <jorge.andre.silva.fernandes@gmail.com>
2021-12-26 19:11:34 +0000

    update

--- OpenCV/Parking Spaces/space_picker.py
+++ OpenCV/Parking Spaces/space_picker.py
@@ -1,6 +1,8 @@
 import cv2
 import pickle
 
+width, height = 107, 48  # 157-50, 240-192
+
 
 def mouse_click(events, x, y, flags, param):
     if events == cv2.EVENT_LBUTTONDOWN:  # create space
@@ -12,15 +14,22 @@ def mouse_click(events, x, y, flags, param):
             if x1 < x < x1 + width and y1 < y < y1 + height:
                 pos_list.pop(i)
 
+    with open("CarParkPos", "wb") as f:
+        pickle.dump(pos_list, f)
 
-width, height = 107, 48  # 157-50, 240-192
-pos_list = []
+
+try:
+    with open("CarParkPos", "rb") as f:
+        pos_list = pickle.load(f)
+except:
+    pos_list = []
+
 
 while True:
     img = cv2.imread("carParkImg.png")
 
     for pos in pos_list:
-        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 1)
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
 
     # cv2.rectangle(img, (50, 192), (157, 240), (255, 0, 255), 2)
     cv2.imshow("image", img)
